8c068f398461ed00b69194a73ef9979789d43389
sachin <thesachindhakad@gmail.com>
2023-06-27 15:58:46 +0530

    hrms complate project

---
+++ hrms/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+_name(request,staff_id,twh):
+#     a1 = Attendance.objects.get(pk=staff_id)
+#     a1.twh = twh
+#     a1.save()
+#     return HttpResponse('aadfdfadsfasdfadsfadsfasdsdf')
+# def display_employee(request, staff_id):
+#     employee = Attendance.objects.get(pk=staff_id)
+#     value = employee.value
+#     context = {
+#         'employee': employee,
+#         'value': value
+#     }
+#     return HttpResponse('faeffewff')
+#     print(context)
